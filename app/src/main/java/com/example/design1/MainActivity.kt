a162fb47ea64ba37af49b136c573070f82135871
Ahmed Hasic <ahmed.hasic@stashcoding.com>
2022-09-02 17:04:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/design1/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.design1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import de.hdodenhof.circleimageview.CircleImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<RecyclerView>(R.id.recyclerView).apply {
+            adapter = MessageAdapter(
+                listOf(
+                    Message(
+                        R.drawable.profile,
+                        "This is a salfe",
+                        "I can send you message",
+                        "2 Aug 2022",
+                        "09:00 AM"
+                    ),
+                    Message(
+                        R.drawable.profile,
+                        "This is a salfe",
+                        "I can send you message",
+                        "2 Aug 2022",
+                        "09:00 AM"
+                    ),
+                    Message(
+                        R.drawable.profile,
+                        "This is a salfe",
+                        "I can send you message",
+                        "2 Aug 2022",
+                        "09:00 AM"
+                    ),Message(
+                        R.drawable.profile,
+                        "This is a salfe",
+                        "I can send you message",
+                        "2 Aug 2022",
+                        "09:00 AM"
+                    ),Message(
+                        R.drawable.profile,
+                        "This is a salfe",
+                        "I can send you message",
+                        "2 Aug 2022",
+                        "09:00 AM"
+                    )
+                )
+            )
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+}
+
+data class Message(
+    val imageUrl: Int,
+    val title: String,
+    val text: String,
+    val date: String,
+    val time: String
+)
+
+class MessageAdapter(
+    val items: List<Message>
+) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val image = view.findViewById<CircleImageView>(R.id.imageView)
+        val title = view.findViewById<TextView>(R.id.this_is_a)
+        val text = view.findViewById<TextView>(R.id.i_can_send)
+        val date = view.findViewById<TextView>(R.id.august_2)
+        val time = view.findViewById<TextView>(R.id.am)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val message = items[position]
+
+        holder.image.setImageResource(message.imageUrl)
+        holder.title.text = message.title
+        holder.text.text = message.text
+        holder.date.text = message.date
+        holder.time.text = message.time
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
